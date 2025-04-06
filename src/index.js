880064c63b772df803814809fff50480d4a78798
ankitshahdeo <ankitshahdeo29@gmail.com>
2025-04-06 20:19:43 +0530

    Added Airplanes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const { PORT } =require('./config/serverconfig');
 
 const ApiRoutes = require('./routes/index');
 
+const db= require('./models/index');
+//const {Airplane} = require('./models/index');
+
 const setupAndStartServer =async () => {
 
        //create the express object
@@ -16,8 +19,23 @@ const setupAndStartServer =async () => {
        
        app.listen(PORT, async()=>{
         console.log(`Server started at ${PORT}`);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter: true});
+        }
+       //  const city = await City.findOne({
+       //        where: {
+       //               id: 6
+       //        }
+       //  });
+       //const airports = await city.getAirports();
+       //  const newAirport= await Airport.findOne({
+       //        where: {
+       //               id:5
+       //        }
+       //  });
+       //  await city.addAirport(newAirport);
 
-        
+
        });
 }
 
